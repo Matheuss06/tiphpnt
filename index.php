4e8a7743ea7a421bd4d78567a65b73d73c21ce2b
Matheus de Castro Souza <matheuscs006@gmail.com>
2023-01-18 17:20:16 -0300

    Conexão e index

--- index.php
+++ index.php
@@ -9,7 +9,29 @@
     <link rel="stylesheet" href="css/estilo.css">
     <title>Chuleta Quente Churrascaria</title>
 </head>
-<body>
-    <h3 class='text-danger bg-success'>Index</h3>
+<body class="fundofixo">
+
+    <!-- Area de menu -->
+    <?php include 'menu_publico.php'; ?>
+    <a name="home">&nbsp;</a>
+    <main class="container">
+
+         <!-- Area de carousel -->
+         <?php include 'carousel.php'; ?>
+
+         <!-- Area de destaque -->
+        <a name="destaques">&nbsp;</a>
+        <?php include 'produtos_destaque.php'; ?>
+
+        <!-- Area geral de produtos -->
+        <a name="produtos">&nbsp;</a>
+        <?php include 'produtos_geral.php'; ?>
+
+        <!-- rodapé -->
+        <footer class="panel-footer" style="background: none;">
+            <?php include 'rodape.php'; ?>
+            <a name="contato"></a>
+        </footer>
+    </main>
 </body>
 </html>
