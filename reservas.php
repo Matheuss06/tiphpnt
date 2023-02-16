6011bf5d87806791ac43b36247403a92be544ba8
Matheus de Castro Souza <matheuscs006@gmail.com>
2023-02-16 11:14:07 -0300

    regras no reservas.php e botao para reservar

--- reservas.php
+++ reservas.php
@@ -11,6 +11,7 @@
     <?php include 'menu_publico.php'; ?>
 <div class="container">
 
+    <!-- Botão para voltar á página anterior -->
     <h2 class="breadcrumb alert-danger">
         <a href="javascript:window.history.go(-1)" class="btn btn-danger">
             <span class="glyphicon glyphicon-chevron-left"></span>
@@ -18,9 +19,18 @@
         <strong> Regras da Reserva </strong>
     </h2>
 
+    <!-- Regras da Reserva -->
     <div class="breadcrumb">
         <h5>• Para cadastrar uma reserva o tempo mínimo será 48 horas de antecedência</h5>
         <h5>• Apenas um pedido de reserva por dia para um mesmo cpf </h5>
+        <h5>• A reserva pode ser feita em até 90 dias</h5>
+        <h5>• Para efetuar a reserva o usuario deverá utilizar o nome completo, CPF e email</h5>
+
+        <div class="flex-center mt-3">
+            <a class="btn btn-danger" href="reservado.php">
+                <span class="glyphicon glyphicon-pencil">&nbsp;RESERVAR</span>
+            </a>
+        </div>
     </div>
 </div>
 </body>
