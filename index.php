afd79c0cdc2f5f316fc0dddac54a0a459401eade
Matheus de Castro Souza <matheuscs006@gmail.com>
2023-02-17 10:57:09 -0300

    Script do angular adcionado

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/estilo.css">
+    <!-- Script do Angular -->
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
     <title>Churrascaria Fornalha</title>
 </head>
 <body class="fundofixo">
